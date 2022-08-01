0bb0a608d8a2f54853c4ee695ec7bda059b8e6cf
rhirosh <rhirosh@gmail.com>
2022-08-01 19:32:27 -0300

    first commit

---
+++ src/test/java/com/trackhour/app/AppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trackhour.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+class AppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
